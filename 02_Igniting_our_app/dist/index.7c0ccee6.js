cbf8271e3478a7cba868a7decdaef17ac0445fcb
SayedAmmar7 <sayedammarhassan@gmail.com>
2023-03-31 12:03:21 +0530

    Added 02_Igniting_our_app

--- 02_Igniting_our_app/dist/index.7c0ccee6.js
+++ 02_Igniting_our_app/dist/index.7c0ccee6.js
@@ -4,22 +4,18 @@
 // var headingText = document.createTextNode("My New Heading");
 // newHeading.appendChild(headingText);
 var parentElement = document.getElementById("root");
-parentElement.appendChild(newHeading); */
-
-// using react
+parentElement.appendChild(newHeading); */ // using react
 /*   const heading = React.createElement("h1", {}, "Hello Hello EveryOne");
       const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading); */
-
-const heading1 = React.createElement("h1", {}, "Hello EveryOne");
+      root.render(heading); */ const heading1 = React.createElement("h1", {}, "Hello EveryOne");
 const heading2 = React.createElement("h2", {}, "Hello Hello EveryOne");
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading2
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(container);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
